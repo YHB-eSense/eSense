21f02ae99deaf1694de1468ef8ea6d68277b713c
Jonas <nils.fester@gmail.com>
2019-12-04 13:45:51 +0100

    Created Settings and ConnectionPage

--- Karl/Karl/View/SettingPage.xaml.cs
+++ Karl/Karl/View/SettingPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +6,34 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Karl.ViewModel;
 
 namespace Karl.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
+
 	public partial class SettingPage : ContentPage
 	{
+
+		private SettingsPageVM svm;
+
 		public SettingPage()
 		{
 			InitializeComponent();
+			svm = new SettingsPageVM();
+		}
+
+		async void OnChangedLanguage(object sender, EventArgs args)
+		{
+			svm.changedLanguage("eng");
+		}
+		async void OnChangedDeviceName(object sender, EventArgs args)
+		{
+			svm.changedDeviceName("Name");
+		}
+		async void OnResetSteps(object sender, EventArgs args)
+		{
+			svm.resetSteps();
 		}
 	}
 }
---
+++ Karl/Karl/ViewModel/SettingsPageVM.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Karl.ViewModel
+{
+	class SettingsPageVM
+	{
+		public void changedDeviceName(String deviceName) {
+
+		}
+
+		public void changedLanguage(String language)
+		{
+
+		}
+
+		public void resetSteps()
+		{
+
+		}
+
+	}
+}
