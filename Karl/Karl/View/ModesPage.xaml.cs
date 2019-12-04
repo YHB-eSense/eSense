8fcc3fcb38769b9052e60635d6bac45ebafe4bde
Maximilian Burzer <maxi.burzer@gmail.com>
2019-12-04 01:57:48 +0100

    added classes to view

---
+++ Karl/Karl/View/ModesPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Karl
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModesPage : ContentPage
+    {
+        public ModesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled_1(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Switch_Toggled_2(object sender, ToggledEventArgs e)
+        {
+
+        }
+    }
+}
