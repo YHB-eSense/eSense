d19de37129f33badf1622f93aaba1fa4cccdf97f
Maximilian Burzer <maxi.burzer@gmail.com>
2019-12-04 12:29:28 +0100

    changed modespage view and viewmodel

--- Karl/Karl/View/ModesPage.xaml.cs
+++ Karl/Karl/View/ModesPage.xaml.cs
@@ -6,25 +6,43 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Karl.ViewModel;
 
 namespace Karl.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ModesPage : ContentPage
 	{
+		ModesVM modesvm;
+
 		public ModesPage()
 		{
 			InitializeComponent();
+			modesvm = new ModesVM();
 		}
 
-		private void Switch_Toggled(object sender, ToggledEventArgs e)
+		private void MM_Switch_Toggled(object sender, ToggledEventArgs e)
 		{
-
+			if(e.Value == true)
+			{
+				modesvm.MotivationModeOff();
+			}
+			else
+			{
+				modesvm.MotivationModeOn();
+			}
 		}
 
-		private void Switch_Toggled_1(object sender, ToggledEventArgs e)
+		private void AM_Switch_Toggled(object sender, ToggledEventArgs e)
 		{
-
+			if (e.Value == true)
+			{
+				modesvm.MotivationModeOff();
+			}
+			else
+			{
+				modesvm.MotivationModeOn();
+			}
 		}
 	}
 }
---
+++ Karl/Karl/ViewModel/ModesVM.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Karl.ViewModel
+{
+	class ModesVM
+	{
+		public void MotivationModeOn()
+		{
+
+		}
+
+		public void MotivationModeOff()
+		{
+
+		}
+
+		public void AutoStopModeOn()
+		{
+
+		}
+
+		public void AutStopModeOff()
+		{
+
+		}
+	}
+}
