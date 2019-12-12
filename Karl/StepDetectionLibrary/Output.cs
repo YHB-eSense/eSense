3797a4374a15b01a81dc1294a601110999832fa4
Yunus Demirag <yunus.demirag@student.kit.edu>
2019-12-12 14:45:55 +0100

    Added Output Observable

--- Karl/StepDetectionLibrary/Output.cs
+++ Karl/StepDetectionLibrary/Output.cs
@@ -4,8 +4,31 @@ using System.Text;
 
 namespace StepDetectionLibrary
 {
-	class Output
+	public class Output
 	{
 
+	}
+	public class OutputManager : IObservable<Output>
+	{
+		public static OutputManager SingletonOutputManager
+		{
+			get
+			{
+				if (SingletonOutputManager == null)
+				{
+					SingletonOutputManager = new OutputManager();
+					return SingletonOutputManager;
+				}
+				else
+				{
+					return SingletonOutputManager;
+				}
+			}
+			private set => SingletonOutputManager = value;
+		}
+		public IDisposable Subscribe(IObserver<Output> observer)
+		{
+			throw new NotImplementedException(); //todo
+		}
 	}
 }
