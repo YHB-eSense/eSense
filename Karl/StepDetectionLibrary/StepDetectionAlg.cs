fa6ff159a640a0d132e1484e35605c85f865fc15
Leo <leoweng@gmx.de>
2019-12-12 16:05:09 +0100

    crazy changes in stepdetectionlib

--- Karl/StepDetectionLibrary/StepDetectionAlg.cs
+++ Karl/StepDetectionLibrary/StepDetectionAlg.cs
@@ -5,9 +5,41 @@ using System.Text;
 
 namespace StepDetectionLibrary
 {
-	class StepDetectionAlg
+	class StepDetectionAlg :IObserver<AccGyroData>, IObservable<Output>
 	{
+		public void OnCompleted()
+		{
+			throw new NotImplementedException();
+		}
 
+		public void OnError(Exception error)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnNext(AccGyroData value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDisposable Subscribe(IObserver<Output> observer)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Output output)
+		{
+			throw new NotImplementedException();
+		}
+		private void Algorithm()
+		{
+
+		}
+
+		private void CalcFreq()
+		{
+
+		}
 	}
 
 }
