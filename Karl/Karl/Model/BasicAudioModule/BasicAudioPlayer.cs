f4fae73cf8020ec23b4cddf6700e1b9a10f4d814
Yunus Demirag <yunus.demirag@student.kit.edu>
2019-12-20 10:06:08 +0100

    Last changes from Yunus

--- Karl/Karl/Model/BasicAudioModule/BasicAudioPlayer.cs
+++ Karl/Karl/Model/BasicAudioModule/BasicAudioPlayer.cs
@@ -7,22 +7,6 @@ namespace Karl.Model
 	
 	sealed class BasicAudioPlayer : IAudioPlayerImpl
 	{
-		private static BasicAudioPlayer _singletonBasicAudioPlayer;
-		public static BasicAudioPlayer SingletonBasicAudioPlayer
-		{
-			get
-			{
-				if (_singletonBasicAudioPlayer == null)
-				{
-					_singletonBasicAudioPlayer = new BasicAudioPlayer();
-					return _singletonBasicAudioPlayer;
-				}
-				else
-				{
-					return _singletonBasicAudioPlayer;
-				}
-			}
-		}
 
 		private BasicAudioPlayer()
 		{
