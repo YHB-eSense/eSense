6daf45630f5b56c8d6e73f034c28fe6bb183d032
Leo <leoweng@gmx.de>
2020-01-06 13:41:11 +0100

    implemented a stepdetection alg

--- Karl/StepDetectionLibrary/StepDetectionAlg.cs
+++ Karl/StepDetectionLibrary/StepDetectionAlg.cs
@@ -10,7 +10,7 @@ namespace StepDetectionLibrary
 	/// </summary>
 	class StepDetectionAlg : IObserver<AccGyroData>, IObservable<Output>
 	{
-		private List<IObserver<Output>> observers;
+		private List<IObserver<Output>> _observer;
 		/// <summary>
 		/// method if provider finished sending data
 		/// </summary>
@@ -38,15 +38,15 @@ namespace StepDetectionLibrary
 		}
 
 		/// <summary>
-		/// method to add observers to stepdetectionalg
+		/// method to add _observer to stepdetectionalg
 		/// </summary>
 		/// <param name="observer">object that wants to observe stepdetectionalg</param>
 		/// <returns>disposable for unsubscribing</returns>
 		public IDisposable Subscribe(IObserver<Output> observer)
 		{
-			if (!observers.Contains(observer))
-				observers.Add(observer);
-			return new Unsubscriber(observers, observer);
+			if (!_observer.Contains(observer))
+				_observer.Add(observer);
+			return new Unsubscriber(_observer, observer);
 
 		}
 
@@ -72,24 +72,50 @@ namespace StepDetectionLibrary
 		}
 
 		/// <summary>
-		/// method to update observers
+		/// method to update _observer
 		/// </summary>
 		/// <param name="output">new data thats been calculated by the algorithm</param>
 		public void Update(Output output)
 		{
-			foreach (var observer in observers)
+			foreach (var observer in _observer)
 			{
 				observer.OnNext(output);
 			}
 		}
 
 		/// <summary>
-		/// method with algorithm to detect steps from acceleration and gyrodata
+		/// method with algorithm to detect steps from acceleration and GyroData
 		/// </summary>
 		/// <param name="data">acceleration and gyro data</param>
 		private Output StepDetecAlg(AccGyroData data)
 		{
-			throw new NotImplementedException();
+			int length = AccGyroData.DATALENGTH;
+			AccData accdata = data.AccData;
+			const double AVGMAG = 10; // value needs to be tested
+			const double THRESHHOLD = 10; //value needs to be tested
+			int stepcount = 0;
+			Boolean threshhold_passed = false;
+
+			double[] netmag = new double[length];
+			for (int i = 0; i < length; i++)
+			{
+				netmag[i] = Math.Sqrt((accdata.Xacc[i]) * (accdata.Xacc[i]) + (accdata.Yacc[i]) * (accdata.Yacc[i]) + (accdata.Zacc[i]) * (accdata.Zacc[i])) - AVGMAG;
+			}
+
+			for (int i = 0; i < length; i++)
+			{
+				if (netmag[i] > THRESHHOLD)
+				{
+					threshhold_passed = true;
+				} else if (threshhold_passed) {
+					threshhold_passed = false;
+					stepcount++;
+				}
+			}
+
+			Output output = new Output(stepcount/(length/50), stepcount);
+
+			return output;
 		}
 	}
 
