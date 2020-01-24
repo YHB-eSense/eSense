4d189126cef6f433b256a735c2a377cafe2669d1
Jonas <nils.fester@gmail.com>
2020-01-24 17:17:30 +0100

    Implemented getting users playlists and devices

--- Karl/Karl/Model/SpotifyAudioModule/eSenseSpotifyWebAPI.cs
+++ Karl/Karl/Model/SpotifyAudioModule/eSenseSpotifyWebAPI.cs
@@ -2,6 +2,7 @@ using Newtonsoft.Json;
 using SpotifyAPI.Web;
 using SpotifyAPI.Web.Models;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using Xamarin.Auth;
@@ -29,19 +30,20 @@ namespace Karl.Model
 
 		private const string CLIENT_ID = "cf74e3a8655c4a03b405d2d52c9193cf";
 		private const string CLIENT_SECRET = "a9b3b53610484638a35a91da896ccae0";
-		private const string SCOPE = "playlist-read-private";
+		private const string SCOPE = "user-read-playback-state";
 		private const string AUTHORIZE_URI = @"https://accounts.spotify.com/authorize";
 		private const string REDIRECT_URI = @"karl1.companyname.com:/oauth2redirect";
 		private const string ACCESSTOKEN_URI = @"https://accounts.spotify.com/api/token";
 		private Account _acc;
 		private HttpClient _client;
+		private OAuth2Authenticator auth;
 
 		public async void Auth()
 		{
 			Uri AuthURI = new Uri(AUTHORIZE_URI);
 			Uri RedirectURI = new Uri(REDIRECT_URI);
 			Uri AccessTokenUri = new Uri(ACCESSTOKEN_URI);
-			OAuth2Authenticator auth = new OAuth2Authenticator(
+			auth = new OAuth2Authenticator(
 				clientId: CLIENT_ID,
 				clientSecret: CLIENT_SECRET,
 				scope: SCOPE,
@@ -49,7 +51,7 @@ namespace Karl.Model
 				redirectUrl: RedirectURI,
 				accessTokenUrl: AccessTokenUri);
 			auth.Completed += OnAuthAsync;
-
+			
 			OAuthLoginPresenter presenter = new OAuthLoginPresenter();
 			
 			presenter.Login(auth);
@@ -61,8 +63,36 @@ namespace Karl.Model
 		{
 			if (!args.IsAuthenticated) throw new HttpRequestException("Authentication failed!");
 			_acc = args.Account;
+			string[] serial = _acc.Serialize().Split('&');
+			string accessToken = serial[1].Split('=')[1];
+			_client.DefaultRequestHeaders.Authorization = new
+				System.Net.Http.Headers.AuthenticationHeaderValue(accessToken);
+			SpotifyWebAPI api = new SpotifyWebAPI
+			{
+				AccessToken = accessToken,
+				TokenType = "Bearer"
+			};
+			PrivateProfile profile = await api.GetPrivateProfileAsync();
+			if (!profile.HasError())
+			{
+				Console.WriteLine(profile.DisplayName);
+				List <SimplePlaylist> playlists = api.GetUserPlaylists(profile.Id).Items;
+				foreach (var playlist in playlists) {
+					Debug.WriteLine(playlist.Name.ToString());
+				}
+				List <Device> devices = api.GetDevices().Devices;
+				Debug.WriteLine(devices.Count);
+				foreach (var device in devices) {
+					Debug.WriteLine(device.Name+ " "+ device.IsActive);
+				}
+				
+			}
+		
+			/*
 			HttpResponseMessage response = await _client.GetAsync("https://api.spotify.com/v1/albums/{6rqhFgbbKwnb9MLmUQDhG6}");
-			Debug.WriteLine("asd"+JsonConvert.DeserializeObject<Token>(response.ToString()).ToString());
+			string responseBody = await response.Content.ReadAsStringAsync();
+			string res = JsonConvert.SerializeObject(responseBody);
+			Debug.WriteLine(res);*/
 		}
 
 
