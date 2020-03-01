95f5dda83a940ab0510b5c8c3c7fb306ddbe3f24
Yunus Demirag <yunus.demirag@student.kit.edu>
2020-03-01 15:30:23 +0100

    Testing

--- Karl/UnitTesting/ModelTests/BasicLibTest.cs
+++ Karl/UnitTesting/ModelTests/BasicLibTest.cs
@@ -1,8 +1,11 @@
+using Karl.Data;
+using Moq;
 using System;
 using System.Diagnostics;
 using System.Reflection;
 using Xunit;
 using Xunit.Sdk;
+using Karl.Model;
 
 namespace UnitTesting.ModelTests
 {
@@ -18,6 +21,7 @@ namespace UnitTesting.ModelTests
 		public void AddSongTest()
 		{
 
+
 		}
 
 	}
@@ -26,14 +30,21 @@ namespace UnitTesting.ModelTests
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 	public class TestBeforeAfter : BeforeAfterTestAttribute
 	{
+		System.Reflection.FieldInfo Instance;
 
 		public override void Before(MethodInfo methodUnderTest)
 		{
+			Instance = typeof(BasicAudioTrackDatabase).GetField("_singletonDatabase", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+			Mock<BasicAudioTrackDatabase> mockSingleton = new Mock<BasicAudioTrackDatabase>();
+			Instance.SetValue(null, mockSingleton.Object);
 			Debug.WriteLine(methodUnderTest.Name);
 		}
 
 		public override void After(MethodInfo methodUnderTest)
 		{
+			Instance.SetValue(null, null);
+
 			Debug.WriteLine(methodUnderTest.Name);
 		}
 	}
