35afe5aca178db056c6ad356de1827175c76b5f5
Maximilian Burzer <maxi.burzer@gmail.com>
2020-03-03 18:45:06 +0100

    added some tests regarding vm

---
+++ Karl/UnitTesting/ViewModelTests/AudioPlayerPageVMTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting.ViewModelTests
+{
+	public class AudioPlayerPageVMTests
+	{
+		[Fact]
+		public void PausePlayCommandTest() { }
+
+		[Fact]
+		public void PlayPrevCommandTest() { }
+
+		[Fact]
+		public void PlayNextCommandTest() { }
+
+		[Fact]
+		public void PositionDragStartedCommandTest() { }
+
+		[Fact]
+		public void PositionDragCompletedCommandTest() { }
+
+	}
+}
---
+++ Karl/UnitTesting/ViewModelTests/MainPageVMTests.cs
@@ -0,0 +1,38 @@
+using Karl.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting.ViewModelTests
+{
+	public class MainPageVMTests
+	{
+		[Fact]
+		public void AudioPlayerPageCommandTest() { }
+
+		[Fact]
+		public void AudioLibPageCommandTest() { }
+
+		[Fact]
+		public void TryConnectCommandTest() { }
+
+		[Fact]
+		public void ModesPageCommandTest() { }
+
+		[Fact]
+		public void SettingsPageCommandTest() { }
+
+		[Fact]
+		public void HelpCommandTest()
+		{
+			var vm = new MainPageVM();
+			vm.HelpVisible = false;
+			vm.HelpCommand.Execute(null);
+			Assert.True(vm.HelpVisible);
+		}
+
+	}
+}
