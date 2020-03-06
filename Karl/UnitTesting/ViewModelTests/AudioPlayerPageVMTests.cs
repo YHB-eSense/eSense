25b4345a64c9bea72337503dc8629f63cfae155b
Jonas <nils.fester@gmail.com>
2020-03-06 09:19:10 +0100

    Fixed Merge

--- Karl/UnitTesting/ViewModelTests/AudioPlayerPageVMTests.cs
+++ Karl/UnitTesting/ViewModelTests/AudioPlayerPageVMTests.cs
@@ -1,3 +1,6 @@
+using Karl.Model;
+using Karl.ViewModel;
+using Moq;
 using Xunit;
 
 namespace UnitTesting.ViewModelTests
@@ -5,19 +8,38 @@ namespace UnitTesting.ViewModelTests
 	public class AudioPlayerPageVMTests
 	{
 		[Fact]
-		public void PausePlayCommandTest() { }
+		public void PausePlayCommandTest() {
+			
+			
+		}
 
 		[Fact]
-		public void PlayPrevCommandTest() { }
+		public void PlayPrevCommandTest() {
+
+		}
 
 		[Fact]
 		public void PlayNextCommandTest() { }
 
 		[Fact]
-		public void PositionDragStartedCommandTest() { }
+		public void PositionDragStartedCommandTest() {
+
+		}
 
 		[Fact]
-		public void PositionDragCompletedCommandTest() { }
+		public void PositionDragCompletedCommandTest() {
+			//AudioPlayerPageVM apVM = new AudioPlayerPageVM();
+			var vm = new Mock<AudioPlayerPageVM_New>();
+			vm.Setup(x => x.AudioTrack).Returns(new TestAudioTrack());
+			vm.Object.PositionDragCompletedCommand.Execute(0.5);
+			Assert.Equal(vm.Object.TimePlayed,"50");
+		}
+
+		public class AudioPlayerPageVM_New : AudioPlayerPageVM {
+			protected override void InitializeSingletons() {}
+			
+		}
 
 	}
+
 }
