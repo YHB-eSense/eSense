ad3528c7dee19248344f1259c6dd25b98133d868
Tims777 <timzi777@gmail.com>
2020-03-19 10:50:19 +0100

    Improve debug output of EarableUtility.Reconnect

--- Karl/EarableLibraryTestApp/EarableLibraryTestApp/EarableUtility.cs
+++ Karl/EarableLibraryTestApp/EarableLibraryTestApp/EarableUtility.cs
@@ -8,11 +8,11 @@ namespace EarableLibraryTestApp
 	{
 		public static async Task Reconnect(IEarable earable)
 		{
-			await earable.DisconnectAsync();
-			Assert.True(!earable.IsConnected());
+			Assert.True(await earable.DisconnectAsync(), "Earable is either disconnected or DisconnectAsync succeeds");
+			Assert.True(!earable.IsConnected(), "Earable disconnected now");
 			await Task.Delay(2000);
-			await earable.ConnectAsync();
-			Assert.True(earable.IsConnected());
+			Assert.True(await earable.ConnectAsync(), "ConnectAsync succeeds");
+			Assert.True(earable.IsConnected(), "Earable connected after connect");
 		}
 	}
 }
