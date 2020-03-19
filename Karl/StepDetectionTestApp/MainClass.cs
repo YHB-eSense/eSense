7fd0e6ae5ab6b1504647d9f7124859f2bfa6817a
Tims777 <timzi777@gmail.com>
2020-03-19 20:54:03 +0100

    Update StepDetectionTestApp

--- Karl/StepDetectionTestApp/MainClass.cs
+++ Karl/StepDetectionTestApp/MainClass.cs
@@ -1,7 +1,6 @@
 using EarableLibrary;
 using StepDetectionLibrary;
 using System;
-using System.Diagnostics;
 using System.IO;
 
 namespace StepDetectionTestApp
@@ -10,20 +9,32 @@ namespace StepDetectionTestApp
 	{
 		public static void Main()
 		{
-			string path = "C:\\Users\\Leo\\Documents\\PSE\\Daten\\16schritte50hz.db";
-			ActivityLog activitylog = new ActivityLog(path);
-			var activityFrames = activitylog.GetData();
+			var inputFiles = Directory.EnumerateFiles("Data", "*.db");
 
 			Input input = new Input();
-			OutputManager.SingletonOutputManager.Subscribe(new Printer());
-			OutputManager.SingletonOutputManager.Log.Reset();
+			OutputManager output = OutputManager.SingletonOutputManager;
+
+			foreach (var file in inputFiles)
+			{
+				ProcessFile(file, input, output);
+			}
+		}
+
+		private static void ProcessFile(string file, Input input, OutputManager output)
+		{
+			output.Log.Reset();
+			ActivityLog activitylog = new ActivityLog(file);
+			var activityFrames = activitylog.GetData();
 
 			for (int i = 0; i < activityFrames.Length; i++)
 			{
 				MotionSensorSample arg = activityFrames[i].ToMotionSensorSample();
 				input.ValueChanged(null, arg);
 			}
-			Debug.WriteLine(Path.Combine(Environment.CurrentDirectory, @"Data\", "6schritte50hz.db"));
+
+			var stepCount = output.Log.CountSteps();
+
+			Console.WriteLine("File '{0}': {1} total steps", file, stepCount);
 		}
 	}
 }
